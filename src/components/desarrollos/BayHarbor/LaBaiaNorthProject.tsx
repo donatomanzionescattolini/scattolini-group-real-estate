c581e833925016c9de8eba1db027a8dae8a34ec0
dmanzione <donato.manz.sc@gmail.com>
2023-11-25 17:00:54 -0500

    la baia banner

--- src/components/desarrollos/BayHarbor/LaBaiaNorthProject.tsx
+++ src/components/desarrollos/BayHarbor/LaBaiaNorthProject.tsx
@@ -6,7 +6,7 @@ export default function LaBaiaNorthProject(){
 
 
 
-    return (<ProjectTemplate properties={<PropiedadesBayHarbor/>} nombre={'la-baia'} caracteristicas={{edificio:<dl><dt><dt>Ubicación</dt><dd>9481 East Bay Harbor Dr, Islas de Bay Harbor, FL 33154, Estados Unidos
-        </dd></dt><dt>Número de Pisos</dt><dd>8</dd><dt>Número de Residencias</dt><dd>57</dd></dl>, residencias:<></>, amenidades:<></>}} introduccion={[]} titulo={"La Baia Norte"} subtitulo={""} numberOfImages={19}/>)
+    return (<ProjectTemplate banner properties={<PropiedadesBayHarbor/>} nombre={'la-baia'} caracteristicas={{edificio:<dl><dt>Ubicación</dt><dd>9481 East Bay Harbor Dr, Islas de Bay Harbor, FL 33154, Estados Unidos
+        </dd><dt>Número de Pisos</dt><dd>8</dd><dt>Número de Residencias</dt><dd>57</dd></dl>, residencias:<></>, amenidades:<></>}} introduccion={[]} titulo={"La Baia Norte"} subtitulo={""} numberOfImages={66}/>)
 
 }

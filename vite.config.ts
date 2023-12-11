a9566e4c34a2b344ec7051fe0a5d3b18bffe4031
dmanzione <donato.manz.sc@gmail.com>
2023-12-11 06:32:51 -0500

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,8 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-
-
-// vite.config.js
 export default defineConfig({
-    build: {
-        // generate .vite/manifest.json in outDir
-        manifest: true,
-        rollupOptions: {
-            // overwrite default .html entry
-            input: 'src/index.tsx',
-        },
-    },
-    plugins: [react()]
+    plugins: [react()],
+
 })
+

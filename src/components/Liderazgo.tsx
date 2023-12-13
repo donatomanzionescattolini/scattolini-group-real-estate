4c795f6c0213e8b53fef5463249d07fede0ff2f2
dmanzione <donato.manz.sc@gmail.com>
2023-12-13 02:58:00 -0500

    monica and liderazgo pages styling

--- src/components/Liderazgo.tsx
+++ src/components/Liderazgo.tsx
@@ -1,36 +1,47 @@
+import { MDBCol, MDBRow } from "mdb-react-ui-kit";
+
 export default function Liderazgo() {
-    return (
-        <div className="row  p-0 m-0">
-            <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 justify-content-center p-5 text-center">
-                <img
-                    src={"https://pagina-mama.s3.amazonaws.com/assets2/elda/blob-9064d3c.jpg"}
-                    alt="Elda"
-                    className="w-50 m-auto img-thumbnail"
-                />
-            </div>
-            <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 p-4">
-                <h4 className="text-center">
-                    Equipo de expertos, Servicio Personalizado
-                </h4>
-                <br></br>
-                <p className=" h-100 mt-5">
-                    Scattolini Group está dirigido por Elda Scattolini, una agente de
-                    bienes raíces con 20 años de experiencia en el mercado del sur de
-                    Florida. Elda ha ayudado a cientos de clientes de diferentes partes
-                    del mundo a alcanzar sus objetivos inmobiliarios, con profesionalismo,
-                    integridad y dedicación.
+  return (
+    <MDBRow className="row  p-0 m-auto">
+      <MDBCol
+        xs={12}
+        sm={12}
+        md={6}
+        lg={6}
+        xl={6}
+        className="justify-content-center p-5 text-center"
+      >
+        <img
+          src={"https://pagina-mama.s3.amazonaws.com/assets2/elda/elda.jpg"}
+          alt="Elda"
+          className="w-50 m-auto img-thumbnail"
+        />
+      </MDBCol>
+      <MDBCol xs={12} sm={12} md={4} lg={4} xl={4} className="px-auto py-5">
+        <h4 className="text-center mt-5">
+          Equipo de expertos, Servicio Personalizado
+        </h4>
+        <br></br>
+        <p style={{ textIndent: "2em" }} className="mt-5">
+          Scattolini Group está dirigido por Elda Scattolini, una agente de
+          bienes raíces con 20 años de experiencia en el mercado del sur de
+          Florida. Elda ha ayudado a cientos de clientes de diferentes partes
+          del mundo a alcanzar sus objetivos inmobiliarios, con profesionalismo,
+          integridad y dedicación.
+        </p>
 
-                </p>
-
-                <p className="text-muted ">
-                    Elda y su equipo de agentes son expertos en el área del sur de
-                    Florida, con un amplio conocimiento de los vecindarios, las escuelas,
-                    las comodidades y las tendencias del mercado. Te guiarán en cada paso
-                    del proceso, desde encontrar la propiedad adecuada hasta negociar el
-                    mejor precio y cerrar el trato de manera eficiente y sin
-                    contratiempos.
-                </p>
-            </div>
-        </div>
-    );
+        <p className="text-muted " style={{ textIndent: "2em" }}>
+          Elda y su equipo de agentes son expertos en el área del sur de
+          Florida, con un amplio conocimiento de los vecindarios, las escuelas,
+          las comodidades y las tendencias del mercado. Te guiarán en cada paso
+          del proceso, desde encontrar la propiedad adecuada hasta negociar el
+          mejor precio y cerrar el trato de manera eficiente y sin
+          contratiempos.
+        </p>
+      </MDBCol>
+      <MDBCol xs={12} sm={12} md={2} lg={2} xl={2}>
+        <br></br>
+      </MDBCol>
+    </MDBRow>
+  );
 }
--- src/components/aliados/ManzioneProperties.tsx
+++ src/components/aliados/ManzioneProperties.tsx
@@ -1,29 +1,49 @@
+import { MDBRow, MDBCol } from "mdb-react-ui-kit";
+import { useLayoutEffect } from "react";
+
 export default function ManzioneProperties() {
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
-    <div className="row  p-0 m-0">
-      <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 justify-content-center p-5 text-center">
+    <MDBRow className=" p-5 m-0">
+      <MDBCol
+        xs={12}
+        sm={12}
+        md={7}
+        lg={7}
+        xl={7}
+        className="align-items-start p-5 text-center"
+      >
         <img
           src={
             "https://pagina-mama.s3.amazonaws.com/assets2/asociados/monica.jpg"
           }
           alt="Elda"
-          className="w-75 m-auto img-thumbnail"
+          className="w-50 m-auto img-thumbnail"
         />
-      </div>
-      <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 p-4">
-        <h2 className="text-center">Manzione Properties</h2>
+      </MDBCol>
+      <MDBCol
+        xs={12}
+        sm={12}
+        md={3}
+        lg={3}
+        xl={3}
+        className=" m-0 p-auto m-auto"
+      >
         <h4 className="text-center">
-          <em> Administración de propiedades en la que puedes confiar</em>
+          Administración de propiedades en la que puedes confiar
         </h4>
 
-        <p className="text-muted ">
+        <br></br>
+        <p className=" mx-0 my-0" style={{ textIndent: 50 }}>
           En nuestra empresa, nos especializamos en la administración de
           propiedades. Nos aseguramos de que cada propiedad esté bien cuidada y
           mantenida, y nos encargamos de todos los detalles para que nuestros
           clientes puedan disfrutar de la tranquilidad de saber que su propiedad
           está en buenas manos.
         </p>
-      </div>
-    </div>
+      </MDBCol>
+    </MDBRow>
   );
 }

00fb6bfad7c8e6873954007c49dcf334fba9c5cd
Donato Manzione <manzione@usc.edu>
2024-09-23 08:32:53 -0500

    pembroke pines done?

--- src/objects/areas/PembrokePines.tsx
+++ src/objects/areas/PembrokePines.tsx
@@ -7,7 +7,7 @@ const PembrokePines = () => {
     PembrokePinesObject.name = "pembroke-pines";
     PembrokePinesObject.slogan = "";
     PembrokePinesObject.titulo = "Pembroke Pines";
-    PembrokePinesObject.descripcion = [""];
+    PembrokePinesObject.descripcion = ["Descubre Pembroke Pines, un lugar donde la calidad de vida y las oportunidades de inversión se encuentran en perfecta armonía. Ubicada en el corazón del sur de Florida, esta vibrante ciudad ofrece una combinación única de comodidades modernas y un ambiente acogedor. Con una infraestructura de primer nivel, excelentes escuelas, y una variedad de parques y espacios recreativos, Pembroke Pines es el lugar perfecto para familias y profesionales que buscan un entorno seguro y dinámico. Además, su proximidad a las principales autopistas y aeropuertos facilita el acceso a las atracciones y playas de Miami y Fort Lauderdale, haciendo de Pembroke Pines una opción inigualable para quienes desean disfrutar de lo mejor de la vida en Florida.","Invertir en Pembroke Pines es una decisión inteligente y rentable. La ciudad ha experimentado un crecimiento constante en el mercado inmobiliario, ofreciendo una amplia gama de propiedades residenciales y comerciales que se adaptan a diferentes necesidades y presupuestos. Desde lujosos condominios hasta acogedoras casas unifamiliares, Pembroke Pines tiene algo para todos. Además, la comunidad empresarial local está en auge, con numerosos centros comerciales, restaurantes y negocios que contribuyen a una economía robusta y diversificada. No pierdas la oportunidad de formar parte de esta próspera comunidad y asegura tu futuro invirtiendo en Pembroke Pines."];
     PembrokePinesObject.numberOfImages = 11;
 
 

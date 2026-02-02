c479a33e7dcbf8a0249aa75d4dcebe490cb34fdd
Donato Manzione <donatomanzionescattolini@gmail.com>
2026-02-02 03:56:24 -0500

    tula

--- src/objects/desarrollos/NorthBayVillage/TulaResidences.tsx
+++ src/objects/desarrollos/NorthBayVillage/TulaResidences.tsx
@@ -33,28 +33,27 @@ TulaResidences.caracteristicas = {
     </>,
     amenidades: <>
         <ul>
-            <li>n</li>
-            <li></li>
+
             <li>Gran entrada en un vestíbulo de diseño llamativo con conserje las 24 horas</li>
-            <li></li>
+          
             <li>Piscina de estilo resort con terraza ajardinada y cabañas que ofrecen vistas al amanecer, al atardecer y nocturnas del horizonte de Miami y la Bahía de Biscayne</li>
-            <li></li>
+          
             <li>Suite de servicios de sexto piso con vistas panorámicas y oportunidades interiores / exteriores para el bienestar y la socialización</li>
-            <li></li>
+          
             <li>Gran habitación con chimenea de mármol, ventanas de piso a techo y vistas a la bahía</li>
-            <li></li>
+          
             <li>Habitación Skyline Club, disponible para reservas privadas</li>
-            <li></li>
+          
             <li>Comedor privado para doce con cocina de calentamiento de chef para eventos atendidos</li>
-            <li></li>
+          
             <li>Gimnasio con 2.100 pies cuadrados de equipo totalmente equipado y de última generación</li>
-            <li></li>
+          
             <li>Edificio que admite mascotas con spa dedicado a mascotas</li>
-            <li></li>
+          
             <li>Garaje de estacionamiento sobre el suelo</li>
-            <li></li>
+          
             <li>Sala de paquetes segura</li>
-            <li></li>
+          
             <li>Almacenamiento privado, disponible a un costo adicional</li>
         </ul>
 
